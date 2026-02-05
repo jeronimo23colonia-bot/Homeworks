40a81448b16d883c71b9425214d88850c6027891
jeroc <jeroc@users.noreply>
2026-02-04 20:24:01 -0500

    modified:   punto2.js

--- punto2.js
+++ punto2.js
@@ -1,3 +1,12 @@
+x = prompt('Ingrese un numero: ');
+elige = prompt('Elija una opcion (1 o 2): ');
+if (elige == '1') {
+    impar1(x);
+} 
+else if (elige == '2') {
+    impar2(x);
+}
+
 function impar1 (x) {
     if (x % 2 !== 0) {
         return true;
