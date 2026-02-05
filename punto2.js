a21e022c770850e127c147bb2fa475dc8acc1e7d
jeroc <jeroc@users.noreply>
2026-02-04 20:16:00 -0500

    new file:   punto.txt 	new file:   punto2.js

---
+++ punto2.js
@@ -0,0 +1,16 @@
+function impar1 (x) {
+    if (x % 2 !== 0) {
+        return true;
+    }    
+    return false;
+}
+
+const impar2 = (x) => {
+    if (x % 2 !== 0) {
+        return true;
+    }       
+    return false;
+}
+
+console.log(impar1(3)); // true
+console.log(impar2(4)); // false
