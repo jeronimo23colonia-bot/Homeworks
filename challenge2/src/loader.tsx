700aaa62fdbf7b27bc457b13ded098c995053a17
jeroc <jeroc@users.noreply>
2026-02-11 21:56:52 -0500

    deleted:    challenge2/src/App.css 	deleted:    challenge2/src/App.tsx 	deleted:    challenge2/src/index.css 	new file:   challenge2/src/loader.tsx 	modified:   challenge2/src/main.tsx

---
+++ challenge2/src/loader.tsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+
+function Loader() {
+    const [loading, setLoading] = useState(true);
+    
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false);
+        }, 2000);
+    }, []);  
+
+    
+    if (loading) {
+        return (
+            <div>
+                <p>cargando contactos...</p>
+            </div>
+        );
+    }
+
+    return (
+        <div>
+        </div>
+    );
+}
+
+export default Loader;  
